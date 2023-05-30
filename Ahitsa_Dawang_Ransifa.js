02d06fc88d81c16a2ae9eadbda270e26fd1636b5
ahitsaadr <127409072+ahitsaadr@users.noreply.github.com>
2023-05-30 22:24:59 +0700

    Add files via upload

---
+++ Ahitsa_Dawang_Ransifa.js
@@ -0,0 +1,10 @@
+function fungsiPertama() {
+    console.log("Ini fungsi pertama");
+  }
+  
+  const variabelPertama = "Ini variabel pertama";
+  
+  function fungsiKedua() {
+    console.log("Ini fungsi kedua");
+  }
+  
