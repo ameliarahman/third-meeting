f47abaae7f6b7950b7af3fe79f1f3c14a93a5be8
laainra <lailaar77@gmail.com>
2023-05-30 11:15:06 +0700

    add funcion, export, import

--- newFolder/V3922026.js
+++ newFolder/V3922026.js
@@ -2,4 +2,4 @@ const { LuasPersegi, KelilingPersegi, LuasLingkaran } = require('../lailaar.js')
 
 console.log(LuasPersegi(15));
 console.log(KelilingPersegi(35));
-console.log(LuasLingkaran(63)); 
+console.log(LuasLingkaran(14)); 
