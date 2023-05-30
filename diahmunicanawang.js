e97c46f4e963aa01f79130f71436afc1eac6298d
diahmunicanawang <diahmunica@gmail.com>
2023-05-30 21:06:40 +0700

    Menambahkan file-file baru

---
+++ diahmunicanawang.js
@@ -0,0 +1,17 @@
+// diah munica nawang.js
+
+function greet() {
+  console.log("Hello, World!");
+}
+
+const name = "Diah Munica Nawang";
+
+function sum(a, b) {
+  return a + b;
+}
+
+module.exports = {
+  greet,
+  name,
+  sum,
+};
