36751750c308d124afe1ab74add0b2d51886827c
melia <meliamadzrongatul12345@gmail.com>
2023-06-02 12:21:46 +0700

    menambahkan folder Melia berisi file melia.js dan index.js dan folfer newFolder berisi V3922030.JS

---
+++ Melia/melia.js
@@ -0,0 +1,11 @@
+function fungsiPertama(kendaraan_roda_2) {
+    return 'Terdapat ${kendaraan_Roda_2} yaitu sepeda'
+  };
+  
+  function fungsiKedua(kendaraan_roda_4) {
+    return 'Terdapat ${kendaraan_Roda_4} yaitu pickup'
+  };
+  
+const pilihan_lain = "masih terdapat berbagai jenis kendaraan"
+
+module.exports = {fungsiPertama, fungsiKedua, pilihan_lain}
