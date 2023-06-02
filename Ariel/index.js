f33d3a9dc6e1401ceab6c49f5b7afda83bd09a77
arielfajar <ariel.fajarh@gmail.com>
2023-06-02 22:36:39 +0700

    berhasil menambahkan file

---
+++ Ariel/index.js
@@ -0,0 +1,7 @@
+// index.js
+
+import { fungsiPertama, fungsiKedua, variabelPertama } from './ArielFajar.js';
+
+// Gunakan fungsi dan variabel yang diimpor
+fungsiPertama();
+console.log(variabelPertama);
