16e35a69229b825ce6e81014fe857b142a79e11a
akupintar788 <muhammadabidin1223@gmail.com>
2023-06-02 21:10:38 +0700

    sampun buu

---
+++ m.Abidin/newFolder/V3922032.js
@@ -0,0 +1,5 @@
+const { variable1, variable2, variable3 } = require('C:\\Tugas\\Abidin\\third-meeting\\m.Abidin\\abidin.js');
+
+console.log(variable1);
+console.log(variable2);
+console.log(variable3);
