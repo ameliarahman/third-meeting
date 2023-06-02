f0685e8ff33270e40610ab3c5ad3fb41c7f1afa1
MahesaAgungSejati <mahesasullivan2004@gmail.com>
2023-06-02 20:31:04 +0700

    add createObject function

---
+++ mahesa/index.js
@@ -0,0 +1,3 @@
+//index.js
+import {fungsi1,fungsi2,variable} from './mahesa.js';
+// gunakan fungsi variabel yang diimport disini
---
+++ mahesa/mahesa.js
@@ -0,0 +1,11 @@
+// mahesa.js
+const fungsi1 = () => {
+    // isi fungsi 1
+};
+
+const fungsi2 = () => {
+    //isi fungsi 2
+};
+
+const variabel = "nilai variable";
+export {fungsi1,fungsi2,variabel};
