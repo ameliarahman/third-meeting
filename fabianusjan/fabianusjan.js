7a3476ed74f5abc5782cc9ba3bdf2a12bbceb8a8
Xsanz1 <fabianusjan4@gmail.com>
2023-06-02 19:43:19 +0700

    add createObject function

---
+++ fabianusjan/fabianusjan.js
@@ -0,0 +1,10 @@
+const fungsi1 = () => {
+    // isi fungsi 1
+};
+
+const fungsi2 = () => {
+    //isi fungsi 2
+};
+
+const variabel = "nilai variable";
+export {fungsi1,fungsi2,variabel};
---
+++ fabianusjan/index.js
@@ -0,0 +1,1 @@
+import {fungsi1,fungsi2,variable} from './fabianusjan.js';
