9137ecead2726fe0c314752aaea9ddf3ca4ee884
Dianalathifa <121172215+Dianalathifa@users.noreply.github.com>
2023-06-02 23:10:37 +0700

    Add files via upload

---
+++ DianaLathifa-TID/V3922016.js
@@ -0,0 +1,9 @@
+// File: /newFolder/V3922016.js
+
+// Impor fungsi dan variabel dari file diaa.js
+const { fungsiSatu, fungsiDua, variabel } = require("./diana.js");
+
+// Panggil fungsi dan gunakan variabel
+fungsiSatu();
+fungsiDua();
+console.log(variabel);
---
+++ DianaLathifa-TID/diana.js
@@ -0,0 +1,20 @@
+// Fungsi pertama
+function fungsiSatu() {
+    console.log("Ini adalah fungsi satu");
+  }
+  
+  // Fungsi kedua
+  function fungsiDua() {
+    console.log("Ini adalah fungsi dua");
+  }
+  
+  // Variabel
+  const variabel = "Ini adalah sebuah variabel";
+  
+  // Ekspor fungsi dan variabel
+  module.exports = {
+    fungsiSatu,
+    fungsiDua,
+    variabel,
+  };
+  
