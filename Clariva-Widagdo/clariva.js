87122c92feec8e3e07445ba0b57d95e71ca40a42
clariva27 <meyclariva@gmail.com>
2023-06-02 10:33:37 +0700

    membuat folder Clariva-Widagdo dengan isi file index.js dan clariva.js serta membuat newFolder di dalamnya dengan isi V3922012.js

---
+++ Clariva-Widagdo/clariva.js
@@ -0,0 +1,11 @@
+function fungsiPertama(kendaraan_roda_2) {
+    return 'Terdapat ${kendaraan_Roda_2} yaitu motor'
+  };
+  
+  function fungsiKedua(kendaraan_roda_4) {
+    return 'Terdapat ${kendaraan_Roda_4} yaitu mobil'
+  };
+  
+const pilihan_lain = "masih terdapat kendaraan yang memiliki 6 dan 8 roda"
+
+module.exports = {fungsiPertama, fungsiKedua, pilihan_lain}
