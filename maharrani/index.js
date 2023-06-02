399cdb742008a2531f92e93754debe3a89bbf3c7
maharranisyy <maharrani72@gmail.com>
2023-06-02 12:46:44 +0700

    menambahkan file baru

---
+++ maharrani/index.js
@@ -0,0 +1,7 @@
+const { LuasPersegi, LuasJajarGenjang, LuasPersegiPanjang } = require('./maharrani.js');
+
+
+
+console.log(LuasPersegi(4));
+console.log(LuasJajarGenjang(4,6));
+console.log(LuasPersegiPanjang(4,9));
---
+++ maharrani/maharrani.js
@@ -0,0 +1,19 @@
+function LuasPersegi(sisi) {
+    return sisi ** 2;
+  }
+
+
+  function LuasJajarGenjang(alas, tinggi) {
+    return alas * tinggi;
+  }
+
+
+  function LuasPersegiPanjang(panjang, lebar) {
+    return panjang * lebar;
+  }
+
+  module.exports = {
+    LuasPersegi,
+    LuasJajarGenjang,
+    LuasPersegiPanjang
+  };
