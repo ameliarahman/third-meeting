ccda384bee5eaa216241f765677aa791c1b19678
Bimo Adji Kusnadi <127374967+adjbims@users.noreply.github.com>
2023-06-02 17:16:14 +0700

    Add files via upload

---
+++ newFolder/V3922010.js
@@ -0,0 +1,7 @@
+// Di dalam file newFolder/V3922010.js
+const { greet, name, age } = require("../bimo-adji-kusnadi");
+
+// Panggil fungsi atau variabel yang telah diimpor
+greet(); // Output: Halo!
+console.log(name); // Output: Bimo Adji Kusnadi
+console.log(age); // Output: 20
