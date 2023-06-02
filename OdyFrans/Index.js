0fbf8fc83bc875587af1d30ea28948d076d7fad7
Franzyishere <odyfranswijaya@gmail.com>
2023-06-02 20:48:03 +0700

    Menambahkan 2 Folder Baru

---
+++ OdyFrans/Index.js
@@ -0,0 +1,3 @@
+//Index.js
+import {fungsi1,fungsi2,variable} from './ody.js';
+// gunakan fungsi variabel yang diimport disini
---
+++ newFolder/V3922037.js
@@ -0,0 +1,3 @@
+// V3922037.js
+import {fungsi1,fungsi2,variabel} from '../ody.js';
+// gunakan fungsi/variabel yang diimport
