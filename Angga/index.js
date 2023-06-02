d831bb10891d5ba1d0ee9fab56a9c074fcfaf139
anggayn <anggayn23@gmail.com>
2023-06-02 20:01:24 +0700

    add createObject function

---
+++ Angga/index.js
@@ -0,0 +1,3 @@
+//index.js
+import {fungsi1,fungsi2,variable} from './Angga.js';
+// gunakan fungsi variabel yang diimport disini
---
+++ Angga/newfolder/V3922006.js
@@ -0,0 +1,3 @@
+// V392206.js
+import {fungsi1,fungsi2,variabel} from '../Angga.js';
+// gunakan fungsi/variabel yang diimport
