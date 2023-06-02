3d4c6d0dc74b6f0b744062e724a41ba1d05333d1
ratripramuditaaa <ratripramudita77@gmail.com>
2023-06-02 19:26:23 +0700

    Ratri/V3922038

---
+++ RatriPramudita/Ratri.js
@@ -0,0 +1,29 @@
+function fullName(name) {
+    var objek ={
+        name :  name,
+    } ;
+
+    console.log(objek);
+}
+
+function nim(nim) {
+    var objek ={
+        nim:  nim,
+    } ;
+
+    console.log(objek);
+}
+
+function user(hobby) {
+    var objek ={
+        hobby:  hobby,
+    } ;
+
+    console.log(objek);
+}
+
+module.exports = {
+   fullName,
+   nim,
+   user
+  };
---
+++ RatriPramudita/V3922038.js
@@ -0,0 +1,7 @@
+const { fullName, nim, user } = require('./Ratri.js');
+
+
+
+console.log(fullName("dita"));
+console.log(nim("V3922089"));
+console.log(user("menyanyi"));git
