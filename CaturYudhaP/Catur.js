beaaff0ab15f78650aa4082932df0ef6c813a850
CaturYudha <csyudha19@gmail.com>
2023-06-02 21:14:44 +0700

    melakukan commit 2 file dengan berisikan 3 file

---
+++ CaturYudhaP/Catur.js
@@ -0,0 +1,5 @@
+const var1 = 'sayur asem';
+const var2 = 'sayur bayam';
+const var3 = 'buggati';
+
+module.exports = {var1, var2, var3};
---
+++ newFolder/V3922011.js
@@ -0,0 +1,5 @@
+const {var1, var2, var3} = require('D:\\third-meeting\\CaturYudhaP\\Catur.js');
+
+console.log(var1)
+console.log(var2)
+console.log(var3)
