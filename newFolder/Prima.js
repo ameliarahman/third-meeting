2a0f3a1b77c7622e4b610a582da146bae664068d
prima288 <kembangprima122@gmail.com>
2023-06-03 10:38:35 +0700

    Tambah File index.js

---
+++ newFolder/Prima.js
@@ -0,0 +1,20 @@
+function greet() {
+    console.log("Hello!");
+}
+
+const name = "Kembang Prima Rossari";
+
+function calculateSum(x, y) {
+    return x + y;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+module.exports = {
+    greet,
+    name,
+    calculateSum,
+    multiply,
+};
